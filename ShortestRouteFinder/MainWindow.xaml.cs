bb6c4ff60566de5fb84de47ca25071d8934c2c60
axamestegui <axamestegui@hotmail.com>
2024-11-06 20:36:29 +0100

    Add project files.

---
+++ ShortestRouteFinder/MainWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using ShortestRouteFinder.ViewModel;
+
+namespace ShortestRouteFinder.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            // Sätt ViewModel som DataContext för fönstret
+            DataContext = new MainViewModel();
+        }
+    }
+}
---
+++ ShortestRouteFinder/ViewModel/MainViewModel.cs
@@ -0,0 +1,44 @@
+﻿using ShortestRouteFinder.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ShortestRouteFinder.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<Route> Routes { get; set; }
+
+        private Route _selectedRoute;
+        public Route SelectedRoute
+        {
+            get => _selectedRoute;
+            set
+            {
+                _selectedRoute = value;
+                OnPropertyChanged(nameof(SelectedRoute));
+            }
+        }
+
+        public MainViewModel()
+        {
+            Routes = new ObservableCollection<Route>(LoadRoutes());
+        }
+
+        private List<Route> LoadRoutes()
+        {
+            var json = File.ReadAllText("routes.json");
+            return JsonConvert.DeserializeObject<List<Route>>(json);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName) =>
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+}
